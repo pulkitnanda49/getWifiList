ee3cb9ccb58802b97daf98fb4004ecbc8f40612e
Pulkit Nanda <pulkit@baliza.io>
2023-06-19 15:12:08 +0530

    feat : get ssid and bssid

--- connected_wifi/ios/Classes/ConnectedWifiPlugin.swift
+++ connected_wifi/ios/Classes/ConnectedWifiPlugin.swift
@@ -3,38 +3,45 @@ import UIKit
 import SystemConfiguration.CaptiveNetwork
 
 public class ConnectedWifiPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "connected_wifi", binaryMessenger: registrar.messenger())
-    let instance = ConnectedWifiPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    case "getWifiList":
-      let connectedWifiList = getAvailableSSIDs();
-      result(connectedWifiList as NSArray)
-    default:
-      result(FlutterMethodNotImplemented)
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "connected_wifi", binaryMessenger: registrar.messenger())
+        let instance = ConnectedWifiPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
     }
-  }
 
-   func getAvailableSSIDs() -> [String] {
-        var ssidList: [String] = []
-        
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        case "getWifiList":
+            let connectedWifiList = getAvailableNetworks()
+            result(connectedWifiList as NSArray)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    func getAvailableNetworks() -> [[String: Any]] {
+        var networkList: [[String: Any]] = []
+
         if let interfaceNames = CNCopySupportedInterfaces() as NSArray? {
             for interfaceName in interfaceNames {
                 if let interfaceName = interfaceName as? String,
-                    let networkInfo = CNCopyCurrentNetworkInfo(interfaceName as CFString) as NSDictionary?,
-                    let ssid = networkInfo[kCNNetworkInfoKeySSID as String] as? String {
-                    ssidList.append(ssid)
+                   
+                   let ssid = networkInfo[kCNNetworkInfoKeySSID as String] as? String,
+                   let bssid = networkInfo[kCNNetworkInfoKeyBSSID as String] as? String {
+                    let network: [String: Any] = [
+                        "ssid": ssid,
+                        "bssid": bssid,
+                        
+                    ]
+                    print("network \(network)")
+                    
+                    networkList.append(network)
                 }
             }
         }
-        
-        return ssidList
+
+        return networkList
     }
 }
