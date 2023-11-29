2e3ceac91d1eb7b919229964aa3658bb0c8379d6
Pulkit Nanda <pulkit@baliza.io>
2023-11-29 18:41:13 +0530

    feat: to get 2.4ghz list of wifi

--- connected_wifi/ios/Classes/ConnectedWifiPlugin.swift
+++ connected_wifi/ios/Classes/ConnectedWifiPlugin.swift
@@ -29,19 +29,38 @@ public class ConnectedWifiPlugin: NSObject, FlutterPlugin {
                 if let interfaceName = interfaceName as? String,
                     let networkInfo = CNCopyCurrentNetworkInfo(interfaceName as CFString) as NSDictionary?,
                    let ssid = networkInfo[kCNNetworkInfoKeySSID as String] as? String,
-                   let bssid = networkInfo[kCNNetworkInfoKeyBSSID as String] as? String {
-                    let network: [String: String] = [
-                        "ssid": ssid,
-                        "bssid": bssid,
-                        
-                    ]
-                    print("network \(network)")
+                   let bssid = networkInfo[kCNNetworkInfoKeyBSSID as String] as? String,
+                   let frequency = getFrequency(for: interfaceName) {
                     
-                    networkList.append(network)
+                    // Check if the frequency is 2.4GHz
+                    if frequency <= 2500 {
+                        let network: [String: String] = [
+                            "ssid": ssid,
+                            "bssid": bssid,
+                            "frequency": "\(frequency) MHz"
+                        ]
+                        print("network \(network)")
+                        networkList.append(network)
+                    }
                 }
+                   
             }
         }
 
         return networkList
+    }
+
+    // Helper function to get the frequency of the connected network
+    func getFrequency(for interfaceName: String) -> Int? {
+        if let supportedInterfaces = CNCopySupportedInterfaces() as? [String] {
+            if supportedInterfaces.contains(interfaceName) {
+                if let interface = CNCopyCurrentNetworkInfo(interfaceName as CFString) as NSDictionary?,
+                    let info = interface[kCNNetworkInfoKeySSID as String] as? [AnyHashable: Any],
+                    let frequency = info[kCNNetworkInfoKeyFrequency as String] as? NSNumber {
+                    return frequency.intValue
+                }
+            }
+        }
+        return nil
     }
 }
